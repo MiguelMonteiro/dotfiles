8b9751b1f2f60a018d8c298f8c3058f88ca4eb64
Miguel Monteiro <m.monteiro@qureight.com>
2025-05-18 16:21:50 +0100

    fix: toggle nabla and markdown independently

--- nvim/lua/markdown.lua
+++ nvim/lua/markdown.lua
@@ -3,14 +3,14 @@ require("render-markdown").setup({
   completions = { lsp = { enabled = true } },
   latex = { enabled = false },
   win_options = { conceallevel = { rendered = 2 } },
-  on = {
-    render = function()
-      require("nabla").enable_virt({ autogen = true })
-    end,
-    clear = function()
-      require("nabla").disable_virt()
-    end,
-  },
+  -- on = {
+  --   render = function()
+  --     require("nabla").enable_virt({ autogen = true })
+  --   end,
+  --   clear = function()
+  --     require("nabla").disable_virt()
+  --   end,
+  -- }, -- best to render nabla independently of markdown plugin
 })
 -- keymaps
 local keymap = vim.keymap.set
@@ -19,9 +19,11 @@ local keymap = vim.keymap.set
 keymap("n", "<leader>vm", function()
   require("render-markdown").toggle()
 end, { desc = "toggle markdown" })
-keymap("n", "<leader>ve", function()
-  require("nabla").popup()
-end, { desc = "toggle equation" })
+
+-- keymap("n", "<leader>ve", function()
+--   require("nabla").popup()
+-- end, { desc = "toggle equation" })
+
 keymap("n", "<leader>va", function()
   require("nabla").toggle_virt()
-end, { desc = "toggle math" })
+end, { desc = "toggle ascii math" })

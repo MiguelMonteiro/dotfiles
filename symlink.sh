d93a0e1e84c708c842920a960796bf3ed7afc324
Miguel Monteiro <mab.mtr@gmail.com>
2026-01-31 19:23:31 +0000

    fix: symlinks

--- symlink.sh
+++ symlink.sh
@@ -1,12 +1,20 @@
 #!/bin/bash
 # symlinks to config files
+# tmux
 ln -sf $HOME/dotfiles/tmux.conf $HOME/tmux.conf
+# alacritty
 mkdir -p $HOME/.config/alacritty
 ln -sf $HOME/dotfiles/alacritty.toml $HOME/.config/alacritty/alacritty.toml
+# neovim
 mkdir -p $HOME/.config/nvim
 ln -sfn $HOME/dotfiles/nvim/* $HOME/.config/nvim
+# homebrew
 ln -sf $HOME/dotfiles/Brewfile $HOME/Brewfile
+# karabiner
 mkdir -p $HOME/.config/karabiner
 ln -sf $HOME/dotfiles/karabiner.json $HOME/.config/karabiner/karabiner.json
+# fish
+mkdir -p $HOME/.config/fish
+ln -sfn $HOME/dotfiles/fish/* $HOME/.config/fish
 
 

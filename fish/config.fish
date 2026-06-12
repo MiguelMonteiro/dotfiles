754b90c542a8ddb496c9c60bab9042aa51a55dc9
Miguel Monteiro <m.monteiro@qureight.com>
2026-06-12 11:52:00 +0100

    fix: conditionals on fish conf

--- fish/config.fish
+++ fish/config.fish
@@ -1,7 +1,12 @@
 # source homebrew path
-eval "$(/opt/homebrew/bin/brew shellenv)"
+if test -x /opt/homebrew/bin/brew
+    eval "$(/opt/homebrew/bin/brew shellenv)"
+end
+
 # source cargo path
-source "$HOME/.cargo/env.fish"
+if test -f "$HOME/.cargo/env.fish"
+    source "$HOME/.cargo/env.fish"
+end
 
 if status is-interactive
 # Commands to run in interactive sessions can go here
@@ -9,4 +14,6 @@ if status is-interactive
 end
 
 # qur8 config
-source ~/qur8-env.fish
+if test -f ~/qur8-env.fish
+    source ~/qur8-env.fish
+end
